5f52742a4b95f47b46b15984074a04fbbbe2c2ed
Ramanan <rubaramanan1997@gmail.com>
2025-01-14 06:59:26 -0500

    players table create query

---
+++ week1/lab_queries/players.sql
@@ -0,0 +1,27 @@
+CREATE TYPE season_stats AS (
+  season INTEGER,
+  pts REAL,
+  ast REAL,
+  reb real,
+  weight INTEGER
+);
+
+
+CREATE TYPE scoring_class as ENUM('star', 'good', 'average', 'bad');
+
+
+CREATE TABLE
+  players (
+    player_name TEXT,
+    height TEXT,
+    college TEXT,
+    country TEXT,
+    draft_year TEXT,
+    draft_round TEXT,
+    draft_number TEXT,
+    seasons season_stats [],
+    scoring_class scoring_class,
+    year_since_last_season INTEGER,
+    current_season INTEGER,
+    PRIMARY KEY(player_name, current_season)
+  );
