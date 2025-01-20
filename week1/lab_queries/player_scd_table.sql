daa88ed19bff8d2261c41a9775fd29e3cc39652f
Ramanan <rubaramanan1997@gmail.com>
2025-01-20 10:50:15 -0500

    player scd update

--- week1/lab_queries/player_scd_table.sql
+++ week1/lab_queries/player_scd_table.sql
@@ -7,3 +7,11 @@ create table player_scd(
     current_season INTEGER,
     PRIMARY KEY(player_name, start_season, end_season)
 );
+
+
+create type scd_type as(
+    is_acitve boolean,
+    scoring_class scoring_class,
+    start_season integer,
+    end_season integer
+);
