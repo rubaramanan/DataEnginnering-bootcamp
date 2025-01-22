cfb603ec7222317ab9f8f75b8333956d5ca331cd
Ramanan <rubaramanan1997@gmail.com>
2025-01-22 16:37:47 -0500

    incremental scd query update

--- week1/lab_queries/incremental_scd_query.sql
+++ week1/lab_queries/incremental_scd_query.sql
@@ -6,7 +6,12 @@ with last_season_scd as (
 ),
 
 historical_scd as(
-    select *
+    select 
+        player_name,
+        is_active,
+        scoring_class,
+        start_season,
+        end_season
     from player_scd
     where current_season = 2021  -- we hard code the 2021 as last season for our convinience.
     and end_season < 2021  -- we collecting the data for before current year(history)
@@ -39,23 +44,57 @@ changed_records as (
         unnest(
             ARRAY[
             ROW(
-                ls.is_acitve,
+                ls.is_active,
                 ls.scoring_class,
                 ls.start_season,
                 ls.end_season
             )::scd_type,
             ROW(
-                ts.is_acitve,
+                ts.is_active,
                 ts.scoring_class,
                 ts.current_season,
                 ts.current_season
             )::scd_type
-        ])
+        ]) as records
     from this_season_data ts
     left join last_season_scd ls
     on ls.player_name = ts.player_name
     where (ls.is_active <> ts.is_active
     or ls.scoring_class <> ts.scoring_class)
+),
+
+unnested_change_records as (
+    select 
+        player_name,
+        (records::scd_type).*
+    from changed_records
+),
+
+new_records as (
+    select 
+        ts.player_name,
+        ts.is_active,
+        ts.scoring_class,
+        ts.current_season as start_season,
+        ts.current_season as end_season
+    from this_season_data ts
+    left join last_season_scd ls
+    on ls.player_name = ts.player_name
+    where ls.player_name is null
 )
 
-select * from unchanged_records limit 100;
+select *, 2022 as current_season from(
+    select * from historical_scd
+
+    union all
+
+    select * from unchanged_records
+
+    union all
+
+    select * from unnested_change_records
+
+    union all
+
+    select * from new_records
+) a
